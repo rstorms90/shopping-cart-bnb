6ebe78ca0ff16783ae3399526cd29be820ef004d
Russ Storms <rstorms90@gmail.com>
2018-11-21 08:46:32 -0700

    fixed priceincents

--- shopping-cart-bnb/src/components/CartItems/CartItem.js
+++ shopping-cart-bnb/src/components/CartItems/CartItem.js
@@ -6,7 +6,7 @@ const CartItem = ({item}) => {
         <div className="collection-item">
             <div className="row">
                 <div className="col-md-8">{item.product.name}</div>
-                <div className="col-md-2">{}</div>
+                <div className="col-md-2">{item.product.priceInCents}</div>
                 <div className="col-md-2">{item.quantity}</div>
             </div>
         </div>
