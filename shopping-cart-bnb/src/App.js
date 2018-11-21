220a8a077da8df1b893ecd7a597746733a507e73
Russ Storms <rstorms90@gmail.com>
2018-11-21 08:35:08 -0700

    done, now to style

--- shopping-cart-bnb/src/App.js
+++ shopping-cart-bnb/src/App.js
@@ -1,15 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react';
+import CartItemsList from './components/CartItems/CartItemsList'
+import NewItem from './components/NewItem/NewItem'
 import CartHeader from './components/CartHeader/CartHeader'
+import CartFooter from './components/CartFooter/CartFooter'
+import Total from './components/Total/Total'
 
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <CartHeader />
-      </div>
-    )
-  }
+    this.state = { 
+        item: 'Mediocre Iron Watch',
+        cart: [
+            { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+            { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+            { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 }
+        ]
+    }  
 }
 
-export default App
+    addItem = ( {myItem, quantity} ) => {
+        let cartCopy = [...this.state.cart]
+        // set next max id
+        const maxId = this.state.cart
+            .reduce((acc, el) => Math.max(acc, el.id), 0)
+        const nextMaxId = maxId + 1
+        // now add the newItem
+        for (let i=0; i < this.state.cart.length; i++) {
+            if (this.state.cart[i].product.id === myItem.id) {
+                let itemClone = {...this.state.cart[i]}
+                itemClone.quantity = itemClone.quantity + parseInt(quantity)
+                cartCopy[i] = itemClone 
+                this.setState({
+                    ...this.state,
+                    cart: cartCopy
+                })
+                return true
+            } 
+        }
+        const newItem = { id: nextMaxId, product: {name: myItem.name, priceInCents: myItem.priceInCents}, quantity: quantity}
+        //create a copy and then incorporate the new Item
+        const newItems = [...this.state.cart, newItem]
+
+        this.setState({
+            ...this.state,
+            cart: newItems
+        })
+    }
+
+    render() {
+
+      return (
+        <div className="container">
+          <CartHeader />
+            <div className="collection-item">
+              <div className="row">
+              <div className="col-md-8">Product</div>
+              <div className="col-md-2">$</div>
+              <div className="col-md-2">#</div>
+            </div>
+          </div>
+          <CartItemsList items={this.state.cart} />
+          <Total items={this.state.cart} />
+          <div className="submitForm">
+          <NewItem newItem={this.newItem} />
+          </div>
+          <CartFooter />
+        </div>
+           )
+       } 
+
+}
