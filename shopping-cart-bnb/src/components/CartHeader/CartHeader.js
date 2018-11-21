2dafb6c71370d5ba6e64a48055e0ec89aad7f5bf
Russ Storms <rstorms90@gmail.com>
2018-11-21 09:39:10 -0700

    added

--- shopping-cart-bnb/src/components/CartHeader/CartHeader.js
+++ shopping-cart-bnb/src/components/CartHeader/CartHeader.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 export default class CartHeader extends Component {
     render() {
+        const navStyle = {
+            backgroundColor: 'teal'
+        }
 
         return (
-            <nav className="navbar navbar-dark bg-primary">
+            <nav style={navStyle} className="navbar navbar-dark bg-primary">
                 <a className="navbar-brand" href="/">Shopping Cart</a>
             </nav>
         )
