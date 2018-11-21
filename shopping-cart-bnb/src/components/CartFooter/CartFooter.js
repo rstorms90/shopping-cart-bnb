db6b7834a821d670c2ca5c8405acdfacebd24106
Russ Storms <rstorms90@gmail.com>
2018-11-20 22:19:26 -0700

    footer

---
+++ shopping-cart-bnb/src/components/CartFooter/CartFooter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CartFooter extends Component {
+    render() {
+
+        return (
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">&copy;</a>
+            </nav>
+        )
+    }
+}
